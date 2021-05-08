7a32695815744a8c9dfcbc16b8a515df53262804
billybutcher <34742984+billybutcher@users.noreply.github.com>
2021-05-08 15:59:52 +0800

    Create DFtoDataset.py

---
+++ DFtoDataset.py
@@ -0,0 +1,12 @@
+import tf.data
+import pandas as pd
+
+def df_to_dataset(dataframe, shuffle=True, batch_size=32):
+  dataframe = dataframe.copy()
+  labels = dataframe.pop('target')
+  ds = tf.data.Dataset.from_tensor_slices((dict(dataframe), labels))
+  if shuffle:
+    ds = ds.shuffle(buffer_size=len(dataframe))
+  ds = ds.batch(batch_size)
+  ds = ds.prefetch(batch_size)
+  return ds
